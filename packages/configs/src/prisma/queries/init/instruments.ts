90d213cb4475b28d8c586ec4449bca519d06f72c
Diego Gomez <diego.gomez@noirlab.edu>
2026-03-31 11:05:41 -0300

    Add GHOST instrument configuration

--- packages/configs/src/prisma/queries/init/instruments.ts
+++ packages/configs/src/prisma/queries/init/instruments.ts
@@ -368,6 +368,19 @@ export const INITIAL_INSTRUMENTS: (Prisma.InstrumentCreateInput & { name: Instru
     isTemporary: false,
     comment: 'Initial configuration',
   },
+  {
+    wfs: 'PWFS2',
+    iaa: 0,
+    issPort: 1,
+    focusOffset: 0.0,
+    name: 'GHOST',
+    ao: false,
+    originX: 0.0,
+    originY: 0.0,
+    extraParams: {},
+    isTemporary: false,
+    comment: 'Initial configuration',
+  },
 ];
 
 export const INITIAL_GEMS_INSTRUMENT: Prisma.GemsInstrumentCreateInput = {
