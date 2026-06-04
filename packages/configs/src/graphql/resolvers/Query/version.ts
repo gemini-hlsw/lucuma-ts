d784a61e1e8454aca82cbfafbdb978a1b6961eee
Hugo van Rijswijk <git@hugovr.nl>
2026-06-04 18:09:05 +0200

    Show schema version in about (#1593)

--- packages/configs/src/graphql/resolvers/Query/version.ts
+++ packages/configs/src/graphql/resolvers/Query/version.ts
@@ -1,8 +1,11 @@
+import schemaPkgJson from '@gemini-hlsw/lucuma-schemas/package.json' with { type: 'json' };
+
 import pkgJson from '../../../../package.json' with { type: 'json' };
 import type { QueryResolvers } from './../../gen/types.generated.ts';
 
 export const version: NonNullable<QueryResolvers['version']> = () => {
   return {
     serverVersion: pkgJson.version,
+    schemaVersion: schemaPkgJson.version,
   };
 };
