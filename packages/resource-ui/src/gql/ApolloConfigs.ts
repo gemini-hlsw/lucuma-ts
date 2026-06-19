1333bcb91c5776a4355ae3a597387ebd71b19590
Hugo van Rijswijk <git@hugovr.nl>
2026-06-19 15:09:38 +0200

    Set up Resource UI deployment to firebase (#1640)

--- packages/resource-ui/src/gql/ApolloConfigs.ts
+++ packages/resource-ui/src/gql/ApolloConfigs.ts
@@ -1,9 +1,28 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { withAbsoluteUri } from '@gemini-hlsw/lucuma-common-ui';
+
+const graphqlEndpoints = {
+  'resource-ae0b7-dev.web.app': 'https://lucuma-resource-dev.lucuma.xyz/resource/graphql',
+  'resource-dev.lucuma.xyz': 'https://lucuma-resource-dev.lucuma.xyz/resource/graphql',
+  localhost: '/resource/graphql',
+
+  'resource-ae0b7-staging.web.app': 'https://lucuma-resource-staging.lucuma.xyz/resource/graphql',
+  'resource-staging.lucuma.xyz': 'https://lucuma-resource-staging.lucuma.xyz/resource/graphql',
+
+  'resource-ae0b7.web.app': 'https://lucuma-resource-dev.lucuma.xyz/resource/graphql',
+  'resource.gemini.edu': 'https://lucuma-resource-dev.lucuma.xyz/resource/graphql',
+} satisfies Record<string, string>;
+
+const defaultGraphqlEndpoint = graphqlEndpoints.localhost;
+
+const uri = graphqlEndpoints[window.location.hostname as keyof typeof graphqlEndpoints] ?? defaultGraphqlEndpoint;
 
 // Create an Apollo Client instance with the specified GraphQL endpoint and cache configuration.
 export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: '/resource/graphql',
-  }),
+  clientAwareness: {
+    name: 'resource-ui',
+    version: import.meta.env.FRONTEND_VERSION,
+  },
+  link: new HttpLink({ uri: withAbsoluteUri(uri) }),
   cache: new InMemoryCache(),
 });
